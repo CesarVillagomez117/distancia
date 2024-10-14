4714e2bd62a3c4950547eba287923fab7b8b8d24
CesarVillagomez117 <viej080310hgtlssa6@btis217.edu.mx>
2024-10-14 07:59:57 -0600

    version 1

---
+++ distancia.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double x, x2;
+    double y, y2;
+    double distancia;
+    cout << "x: ";
+    cin >> x;
+    cout << "x2: ";
+    cin >> x2;
+    cout << "y: ";
+    cin >> y;
+    cout << "y2: ";
+    cin >> y2;
+
+    distancia = sqrt(pow(x2 - x, 2) + pow(y2 - y, 2));
+
+    cout << "La distancia es de: " << distancia << endl;
+
+    return 0;
+}
